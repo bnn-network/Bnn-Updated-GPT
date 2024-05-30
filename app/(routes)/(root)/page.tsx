3ffaf283c301b393aaef4f3377aa8f9ea4890859
DhrishP <parekhdhrish.pg@gmail.com>
2024-05-30 10:59:51 +0530

    fix : streaming text expanding after generation

--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -9,8 +9,8 @@ export default async function Page() {
   return (
     <>
       <AI initialAIState={{ chatId: id, messages: [] }}>
-        <div className="flex flex-col items-center -z-10 min-h-screen justify-center bg-secondary text-primary">
-          <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        <div className="flex  items-center -z-10 min-h-screen justify-center bg-secondary text-primary">
+          <main className="flex flex-col items-center justify-center flex-1 px-2 text-center">
             <h1 className="text-2xl mb-8">Expand Your Genius</h1>{' '}
             <div className="mt-8 w-full max-w-3xl">
               <Chat id={id} />
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 import { ThemeProvider } from '@/components/theme-provider'
-import { Sidebar } from '@/components/sidebar'
 import { Toaster } from '@/components/ui/sonner'
 
 const fontSans = FontSans({
