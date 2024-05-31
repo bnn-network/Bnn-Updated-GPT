a1fb6db6e8354f88bdc999323b809f222261583a
DhrishP <parekhdhrish.pg@gmail.com>
2024-05-31 06:48:46 +0530

    fix : UI formatting issues with answering bot message

--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -10,9 +10,9 @@ export default async function Page() {
     <>
       <AI initialAIState={{ chatId: id, messages: [] }}>
         <div className="flex  items-center -z-10 min-h-screen justify-center bg-secondary text-primary">
-          <main className="flex flex-col items-center justify-center flex-1 px-2 text-center">
+          <main className="flex flex-col items-center justify-center flex-1 px-2 ">
             <h1 className="text-2xl mb-8">Expand Your Genius</h1>{' '}
-            <div className="mt-8 w-full max-w-3xl lg:max-w-4xl">
+            <div className="mt-8 w-full ">
               <Chat id={id} />
             </div>
           </main>
