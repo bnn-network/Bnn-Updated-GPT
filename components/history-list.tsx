aac6df123d21e16bcf993450bdd3e3d2565d4a55
DhrishP <parekhdhrish.pg@gmail.com>
2024-06-01 12:50:54 +0530

    temp fix : removed refresh of page after generation for history update

--- components/history-list.tsx
+++ components/history-list.tsx
@@ -1,8 +1,10 @@
+
 import React, { cache } from 'react'
 import HistoryItem from './history-item'
 import { Chat } from '@/lib/types'
 import { getChats } from '@/lib/actions/chat'
 import { ClearHistory } from './clear-history'
+
 
 type HistoryListProps = {
   userId?: string
@@ -12,9 +14,10 @@ const loadChats = cache(async (userId?: string) => {
   return await getChats(userId)
 })
 
-// Start of Selection
 export async function HistoryList({ userId }: HistoryListProps) {
+
   const chats = await loadChats(userId)
+ 
 
   return (
     <div className="flex flex-col flex-1 space-y-3 h-full">
