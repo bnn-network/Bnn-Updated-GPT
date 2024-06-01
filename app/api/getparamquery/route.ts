97b0b132b4f89a3514d41af14deaeab387b14917
DhrishP <parekhdhrish.pg@gmail.com>
2024-06-01 12:56:04 +0530

    fix : instructions for query generation changed for better results

--- app/api/getparamquery/route.ts
+++ app/api/getparamquery/route.ts
@@ -1,10 +1,10 @@
-import { OpenAI } from '@ai-sdk/openai'
+import { createOpenAI } from '@ai-sdk/openai'
 import { generateText } from 'ai'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
   const { path } = await req.json()
-  const openai = new OpenAI({
+  const openai = createOpenAI({
     baseUrl: process.env.OPENAI_API_BASE,
     apiKey: process.env.OPENAI_API_KEY
   })
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
       {
         role: 'assistant',
         content:
-          'You are helpful assistant , you help users to generate a meaningfull prompt for the paramater query sent by the user without asking any question if the prompt makes sense or not. The query generated should be based on the user input and if it does not make sense then give the prompt as the answer. Please do not ask any question to user , just generate the query based on the user input. example string : "What is happening in US right now? or "What is the weather in New York?'
+          'You are helpful assistant , you help users to generate a meaningful queries for the paramaters of the url sent by the user without asking any question even if the prompt makes sense or not. The query generated should be based on the user input. Please do not ask any question to user , just generate the query based on the user input. example string : "What is happening in US right now? or "What is the weather in New York?'
       },
       {
         role: 'user',
