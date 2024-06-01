37abb60b9672ec5210821d49e26ccbcd551483e3
nayeem6 <nayeem@care-box.com>
2024-06-01 22:45:12 +0600

    message-changed-v1

--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
       <AI initialAIState={{ chatId: id, messages: [] }}>
         <div className="flex  items-center -z-10 min-h-screen justify-center bg-secondary text-primary">
           <main className="flex flex-col items-center justify-center flex-1 px-2 ">
-            <h1 className="text-2xl mb-8">Limitless Discovery</h1>{' '}
+            <h1 className="text-2xl mb-8">Limitless Curiosity</h1>{' '}
             <div className="mt-8 w-full  ">
               <Chat id={id} />
               <Footer />
