8878dc9c8344273aa10bd61f4de11eda039ce133
DhrishP <parekhdhrish.pg@gmail.com>
2024-06-02 18:21:36 +0530

    feat : added SEO for all the static footer pages

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -2,47 +2,58 @@ import Footer from '@/components/footer'
 import '../../styles.css' // This should be the correct relative path
 import Head from 'next/head'
 
+export async function generateMetadata() {
+  return {
+    title: 'Contact BNNGPT - Get in Touch with Our AI Search Engine Team',
+    description: `Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs.`,
+    metadataBase: new URL('https://www.bnngpt.com/contact')
+  }
+}
+
 export default function Page() {
   return (
     <>
-    <div className="inner_page bg-secondary">
-      <Head>
-        <title>
-          Contact BNNGPT - Get in Touch with Our AI Search Engine Team
-        </title>
-        <meta
-          name="description"
-          content="Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs."
-        />
-        <meta
-          property="og:title"
-          content="Contact BNNGPT - Get in Touch with Our AI Search Engine Team"
-        />
-        <meta
-          property="og:description"
-          content="Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs."
-        />
-        <meta property="og:url" content="https://www.bnngpt.com/contact" />
-        <meta property="og:image" content="https://www.bnngpt.com/logo.jpg" />
-        <meta property="og:site_name" content="BNNGPT" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="" />
-        <meta name="twitter:creator" content="" />
-        <meta
-          name="twitter:title"
-          content="Contact BNNGPT - Get in Touch with Our AI Search Engine Team"
-        />
-        <meta
-          name="twitter:description"
-          content="Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs."
-        />
-        <meta name="twitter:image" content="https://www.bnngpt.com/logo.jpg" />
-      </Head>
-      <a href="/" className="logo_div"></a>
-      <h2>Contact Us</h2>
-      <iframe src="https://forms.zohopublic.in/vijendersingh/form/BNNGPT/formperma/BHAVU_QZ00PzHO8Wfnx5gYJ_pZIRndsBaHZxTkN679w"></iframe>
-    </div>
-    <Footer/>
+      <div className="inner_page bg-secondary">
+        <Head>
+          <title>
+            Contact BNNGPT - Get in Touch with Our AI Search Engine Team
+          </title>
+          <meta
+            name="description"
+            content="Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs."
+          />
+          <meta
+            property="og:title"
+            content="Contact BNNGPT - Get in Touch with Our AI Search Engine Team"
+          />
+          <meta
+            property="og:description"
+            content="Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs."
+          />
+          <meta property="og:url" content="https://www.bnngpt.com/contact" />
+          <meta property="og:image" content="https://www.bnngpt.com/logo.jpg" />
+          <meta property="og:site_name" content="BNNGPT" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="" />
+          <meta name="twitter:creator" content="" />
+          <meta
+            name="twitter:title"
+            content="Contact BNNGPT - Get in Touch with Our AI Search Engine Team"
+          />
+          <meta
+            name="twitter:description"
+            content="Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs."
+          />
+          <meta
+            name="twitter:image"
+            content="https://www.bnngpt.com/logo.jpg"
+          />
+        </Head>
+        <a href="/" className="logo_div"></a>
+        <h2>Contact Us</h2>
+        <iframe src="https://forms.zohopublic.in/vijendersingh/form/BNNGPT/formperma/BHAVU_QZ00PzHO8Wfnx5gYJ_pZIRndsBaHZxTkN679w"></iframe>
+      </div>
+      <Footer />
     </>
   )
 }
