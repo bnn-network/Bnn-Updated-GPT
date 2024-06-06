84b10ef2dad3995de31488053fd073049fa79aee
DhrishP <parekhdhrish.pg@gmail.com>
2024-06-06 12:48:15 +0530

    fix: middleware try-catch addition and prisma postinstall removal

--- middleware.ts
+++ middleware.ts
@@ -65,35 +65,46 @@ export default clerkMiddleware(
   async (auth: ClerkMiddlewareAuth, req: NextRequest) => {
     const { pathname } = req.nextUrl
     if (pathname.length === 16) {
-      const segments = pathname.split('/').filter(segment => segment !== '')
-      const firstSegment = segments[0]
-      if (firstSegment.length !== 5) {
-        return NextResponse.redirect(process.env.NEXT_PUBLIC_BASE_URL as string)
-      }
-      const res = await fetch(
-        `https://check.bnngpt.com/get_archived_title?url=https://tr.im/${firstSegment}`
-      )
-      const data = await res.json()
-      if (data.title === null || !data.title) {
-        return NextResponse.redirect(process.env.NEXT_PUBLIC_BASE_URL as string)
-      }
-      const res2 = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/getparamquery`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ path: data.title })
+      try {
+        const segments = pathname.split('/').filter(segment => segment !== '')
+        const firstSegment = segments[0]
+        if (firstSegment.length === 5) {
+          const res = await fetch(
+            `https://check.bnngpt.com/get_archived_title?url=https://tr.im/${firstSegment}`
+          )
+          const data = await res.json()
+          if (data.title === null || !data.title) {
+            return NextResponse.redirect(
+              process.env.NEXT_PUBLIC_BASE_URL as string
+            )
+          }
+          const res2 = await fetch(
+            `${process.env.NEXT_PUBLIC_BASE_URL}/api/getparamquery`,
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ path: data.title })
+            }
+          )
+          const resText = await res2.text()
+          if (resText.length > 0) {
+            return NextResponse.redirect(
+              `${process.env.NEXT_PUBLIC_BASE_URL}?prequery=${resText}`
+            )
+          }
+          return NextResponse.redirect(
+            process.env.NEXT_PUBLIC_BASE_URL as string
+          )
+        } else {
+          return NextResponse.redirect(
+            process.env.NEXT_PUBLIC_BASE_URL as string
+          )
         }
-      )
-      const resText = await res2.text()
-      if (resText.length > 0) {
-        return NextResponse.redirect(
-          `${process.env.NEXT_PUBLIC_BASE_URL}?prequery=${resText}`
-        )
+      } catch (error) {
+        return NextResponse.redirect(process.env.NEXT_PUBLIC_BASE_URL as string)
       }
-      return NextResponse.redirect(process.env.NEXT_PUBLIC_BASE_URL as string)
     }
 
     if (pathname.length === 10) {
