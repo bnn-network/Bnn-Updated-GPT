71664af6702cbb967d64e950a638f0bb0c0b8d73
DhrishP <parekhdhrish.pg@gmail.com>
2024-06-12 15:07:38 +0530

    attempt : to fix client side error using recursive calling of objects

--- lib/agents/query-suggestor.tsx
+++ lib/agents/query-suggestor.tsx
@@ -19,9 +19,10 @@ export async function querySuggestor(
   )
 
   let finalRelatedQueries: PartialRelated = {}
-  await streamObject({
-    model: openAIInstance('gpt-3.5-turbo'),
-    system: `As a professional web researcher, your task is to generate a set of three queries that explore the subject matter more deeply, building upon the initial query and the information uncovered in its search results.
+  try {
+    await streamObject({
+      model: openAIInstance('gpt-3.5-turbo'),
+      system: `As a professional web researcher, your task is to generate a set of three queries that explore the subject matter more deeply, building upon the initial query and the information uncovered in its search results.
 
     For instance, if the original query was "Starship's third test flight key milestones", your output should follow this format:
 
@@ -35,29 +36,29 @@ export async function querySuggestor(
 
     Aim to create queries that progressively delve into more specific aspects, implications, or adjacent topics related to the initial query. The goal is to anticipate the user's potential information needs and guide them towards a more comprehensive understanding of the subject matter.
     Please match the language of the response to the user's language.`,
-    messages,
-    schema: relatedSchema
-  })
-    .then(async result => {
-      for await (const obj of result.partialObjectStream) {
-        if (obj.items) {
-          objectStream.update(obj)
-          finalRelatedQueries = obj
+      messages,
+      schema: relatedSchema
+    })
+      .then(async result => {
+        for await (const obj of result.partialObjectStream) {
+          if (obj.items) {
+            objectStream.update(obj)
+            finalRelatedQueries = obj
+          }
         }
-      }
-    })
-    .catch(async error => {
-      console.error('Error in querySuggestor:', error)
-      const rescursiveResult: any = await querySuggestor(
-        uiStream,
-        messages,
-        selectedModel
-      )
-      return rescursiveResult
-    })
-    .finally(() => {
-      objectStream.done()
-    })
+      })
+      .catch(async error => {
+        console.error('Error in querySuggestor:', error)
+        const rescursiveResult: any = await querySuggestor(
+          uiStream,
+          messages,
+          selectedModel
+        )
+        return rescursiveResult
+      })
+  } finally {
+    objectStream.done()
+  }
 
   return finalRelatedQueries
 }
