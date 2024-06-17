ae5eab91ccb93eac461d47d3be5e2e2cc6785d54
DhrishP <parekhdhrish.pg@gmail.com>
2024-06-17 21:38:36 +0530

    fix : quality of life improvements

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { ModeToggle } from './mode-toggle'
 import HistoryContainer from './history-container'
 import ImageComponent from './image-component'
-import LoginButton from './auth-button'
 import MenuButton from './MenuButton'
 
 
 export const Header: React.FC = () => {
   return (
-    <header className="fixed w-full -z-10 bg-secondary py-1 md:p-2 flex justify-between lg:pr-4 items-center z-10 backdrop-blur md:backdrop-blur-none  md:bg-transparent">
+    <header className="fixed top-2 w-full -z-10 bg-secondary py-1 md:p-2 flex justify-between lg:pr-4 items-center  backdrop-blur md:backdrop-blur-none  md:bg-transparent">
       <ImageComponent />
       <div className="flex gap-0.5 z-10 pr-2 lg:pr-0">
 
