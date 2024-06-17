68ee97a69259a7e7092c508dfe8834afdbbc94f1
DhrishP <parekhdhrish.pg@gmail.com>
2024-06-17 20:46:03 +0530

    fixed scrolling issue

--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -13,9 +13,9 @@ export default async function Page() {
   return (
     <>
       <AI initialAIState={{ chatId: id, messages: [] }}>
-        <div className="flex items-center lg:h-screen  min-h-[90vh] justify-center bg-secondary text-primary">
+        <div className="flex items-center  min-h-screen  justify-center bg-secondary text-primary">
           <main className="flex flex-col items-center justify-center flex-1 px-2 ">
-            <h1 className="text-2xl mb-24 lg:mb-16">Limitless Curiosity</h1>{' '}
+            <h1 className="text-2xl mb-44 lg:mb-32">Limitless Curiosity</h1>{' '}
             <div className="mt-8 w-full  ">
               <Chat id={id} />
               <FooterAI />
