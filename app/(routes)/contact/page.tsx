f41dc3dbfaf69b2f428ecd85437961434e98e4a1
gchahal1982 <108035922+gchahal1982@users.noreply.github.com>
2024-06-27 13:25:33 +0800

    GC Changes

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -1,6 +1,6 @@
 import Footer from '@/components/footer'
 import '../../styles.css' // This should be the correct relative path
-import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 
 export async function generateMetadata() {
   return {
@@ -13,42 +13,30 @@ export async function generateMetadata() {
 export default function Page() {
   return (
     <>
+      <NextSeo
+        title="Contact BNNGPT - Get in Touch with Our AI Search Engine Team"
+        description="Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs."
+        openGraph={{
+          url: 'https://www.bnngpt.com/contact',
+          title: 'Contact BNNGPT - Get in Touch with Our AI Search Engine Team',
+          description:
+            "Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs.",
+          images: [
+            {
+              url: 'https://www.bnngpt.com/og-image.jpg',
+              width: 800,
+              height: 600,
+              alt: 'BNNGPT Open Graph Image'
+            }
+          ],
+          site_name: 'BNNGPT'
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+          site: '@epiphanyaitech' // If you have a Twitter handle for your site
+        }}
+      />
       <div className="inner_page bg-secondary">
-        <Head>
-          <title>
-            Contact BNNGPT - Get in Touch with Our AI Search Engine Team
-          </title>
-          <meta
-            name="description"
-            content="Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs."
-          />
-          <meta
-            property="og:title"
-            content="Contact BNNGPT - Get in Touch with Our AI Search Engine Team"
-          />
-          <meta
-            property="og:description"
-            content="Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs."
-          />
-          <meta property="og:url" content="https://www.bnngpt.com/contact" />
-          <meta property="og:image" content="https://www.bnngpt.com/logo.jpg" />
-          <meta property="og:site_name" content="BNNGPT" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="" />
-          <meta name="twitter:creator" content="" />
-          <meta
-            name="twitter:title"
-            content="Contact BNNGPT - Get in Touch with Our AI Search Engine Team"
-          />
-          <meta
-            name="twitter:description"
-            content="Reach out to BNNGPT for support, inquiries, or feedback. We're here to assist with all your AI search engine needs."
-          />
-          <meta
-            name="twitter:image"
-            content="https://www.bnngpt.com/logo.jpg"
-          />
-        </Head>
         <a href="/" className="logo_div"></a>
         <h2>Contact Us</h2>
         <iframe src="https://forms.zohopublic.in/vijendersingh/form/BNNGPT/formperma/BHAVU_QZ00PzHO8Wfnx5gYJ_pZIRndsBaHZxTkN679w"></iframe>
